d54dbb375cd3d8fa225ac822eca0a0c581dfe2d6
Alexander Yarygin <yarygin.alexander@gmail.com>
2012-11-21 20:47:07 +0400

    added node types pages

---
+++ view/nodeTypePage.h
@@ -0,0 +1,67 @@
+/**
+ *
+ * File: nodeTypePage.h
+ * Description: nodeType widget
+ * Author: Alexander Yarygin <yarygin.alexander@gmail.com>
+ *
+ **/
+
+#ifndef NODETYPEPAGE_H
+#define NODETYPEPAGE_H
+
+#include <QMap>
+#include <QString>
+#include <QLineEdit>
+#include <QGroupBox>
+#include <QComboBox>
+
+#include "moduleParams.h"
+#include "projectParams.h"
+
+namespace Ui {
+    class NodeTypePage;
+}
+
+class NodeTypePage : public QGroupBox
+{
+    Q_OBJECT
+
+public:
+    NodeTypePage(QString name, QList<ModuleDescriptionRaw*> modules);
+    virtual ~NodeTypePage();
+
+    void moduleEnabled(ModuleDescriptionRaw* module);
+    void moduleDisabled(ModuleDescriptionRaw* module);
+
+    void newModule(quint16 moduleID);
+
+private slots:
+
+    void customContextMenuRequested(const QPoint &p);
+    // void removeModule(int row);
+
+    void addModule_toTable_fromCombobox();
+private:
+
+    bool isNodeModule(ModuleDescriptionRaw* module);
+
+    void addModule_toCombobox(ModuleDescriptionRaw* module);
+    void removeModule(ModuleDescriptionRaw* module);
+    void addModule_toTable(ModuleDescriptionRaw* module);
+    void removeModule_fromTable(int row);
+    void removeModule_fromCombobox(int index);
+
+    QString m_name;
+    // module -> moduleID
+    QList<ModuleDescriptionRaw*> m_nodeTypesModules;
+
+    // combobox index -> module
+    QMap<int, ModuleDescriptionRaw*> m_indexes;
+
+    // table row -> module
+    QMap<int, ModuleDescriptionRaw*> m_nodeType;
+
+    Ui::NodeTypePage *m_ui;
+};
+
+#endif // NODETYPEPAGE_H
---
+++ view/nodeTypesPage.cpp
@@ -0,0 +1,131 @@
+/**
+ *
+ * File: nodeTypesPage.cpp
+ * Description: Page of choosing nodeTypes from list
+ * Author: Yarygin Alexander <yarygin.alexander@gmail.com>
+ *
+ **/
+
+#include "nodeTypesPage.h"
+#include "ui_nodeTypesPage.h"
+
+NodeTypesPage::NodeTypesPage(QTreeWidgetItem* treeElement, ProjectTree* projectTree)
+    :m_ui(new Ui::NodeTypesPage), m_selfTreeElement(treeElement), m_projectTree(projectTree)
+{
+    m_ui->setupUi(this);
+
+    connect(m_ui->le_nodeType, SIGNAL(textChanged(QString)),
+            this, SLOT(isNewNodeTypeName(QString)));
+
+    connect(m_ui->b_add, SIGNAL(clicked()),
+            this, SLOT(createNodeTypePage()));
+
+    connect(m_ui->list_nodeTypes, SIGNAL(customContextMenuRequested(const QPoint &)),
+            this, SLOT(customContextMenuRequested(const QPoint &)));
+}
+
+NodeTypesPage::~NodeTypesPage()
+{
+    delete m_ui;
+}
+
+void NodeTypesPage::newNodeType(NodeTypeData* nodeType)
+{
+    createNodeTypePage(nodeType->name);
+    NodeTypePage* page = m_nodeTypes[nodeType->name];
+
+    for (int i = 0; i < nodeType->hardwareModules.size(); i++)
+        page->newModule(nodeType->hardwareModules[i]);
+    for (int i = 0; i < nodeType->softwareModules.size(); i++)
+        page->newModule(nodeType->softwareModules[i]);
+}
+
+void NodeTypesPage::moduleEnabled(ModuleDescriptionRaw* module)
+{
+    m_modules += module;
+
+    foreach(NodeTypePage* page, m_nodeTypes.values())
+        page->moduleEnabled(module);
+}
+
+void NodeTypesPage::moduleDisabled(ModuleDescriptionRaw* module)
+{
+    m_modules.removeOne(module);
+
+    foreach(NodeTypePage* page, m_nodeTypes.values())
+        page->moduleDisabled(module);
+}
+
+void NodeTypesPage::createNodeTypePage(QString nodeTypeName)
+{
+    NodeTypePage* page = new NodeTypePage(nodeTypeName, m_modules);
+
+    m_nodeTypes[nodeTypeName] = page;
+    m_nodeTypesNames += nodeTypeName;
+
+    m_ui->list_nodeTypes->addItem(new QListWidgetItem(nodeTypeName));
+
+    QTreeWidgetItem* ti_page = m_projectTree->addTiWidget(nodeTypeName, m_selfTreeElement);
+    m_projectTree->addPage(ti_page, page);
+}
+
+void NodeTypesPage::createNodeTypePage()
+{
+    QString nodeTypeName = m_ui->le_nodeType->text();
+    createNodeTypePage(nodeTypeName);
+    m_ui->le_nodeType->setText("");
+}
+
+void NodeTypesPage::deleteNodeTypePage(QListWidgetItem* nodeTypeItem)
+{
+    QString nodeTypeName = nodeTypeItem->text();
+
+    deleteNodeTypePage(nodeTypeName);
+
+    m_ui->list_nodeTypes->removeItemWidget(nodeTypeItem);
+    delete nodeTypeItem;
+}
+
+void NodeTypesPage::deleteNodeTypePage(QString nodeTypeName)
+{
+    // TODO: check if user change params
+    NodeTypePage* page = m_nodeTypes[nodeTypeName];
+    m_nodeTypes.remove(nodeTypeName);
+
+    m_projectTree->removePage(page);
+
+    delete page;
+}
+
+void NodeTypesPage::isNewNodeTypeName(QString name)
+{
+    if (m_nodeTypesNames.contains(name))
+        m_ui->b_add->setEnabled(false);
+    if ((!m_nodeTypesNames.contains(name))
+        && (!m_ui->b_add->isEnabled()))
+        m_ui->b_add->setEnabled(true);
+}
+
+void NodeTypesPage::customContextMenuRequested(const QPoint &p)
+{
+    QListWidgetItem* nt_name = m_ui->list_nodeTypes->itemAt(p);
+
+    if (nt_name == NULL)
+        return;
+
+    QMenu menu(m_ui->list_nodeTypes);
+
+    QAction* actionRemove = menu.addAction(tr("&Remove"));
+
+    QAction* a = menu.exec(m_ui->list_nodeTypes->mapToGlobal(p));
+
+    if (a == actionRemove)
+        deleteNodeTypePage(nt_name);
+}
+
+void NodeTypesPage::clean()
+{
+    foreach(QString name, m_nodeTypesNames)
+        deleteNodeTypePage(name);
+    m_ui->list_nodeTypes->clear();
+}
