564ea7c55aa040a7e558cf20e30d167a74197f10
Alexander Yarygin <yarygin.alexander@gmail.com>
2012-12-03 22:55:35 +0400

    added errors panel

---
+++ widgets/errorsPanel.cpp
@@ -0,0 +1,79 @@
+/**
+ *
+ * File: errorsPanel.cpp
+ * Author: Alexander Yarygin <yarygin.alexander@gmail.com>
+ *
+ **/
+
+#include <QMenu>
+
+#include "errorsPanel.h"
+#include "ui_errorsPanel.h"
+
+ErrorsPanel::ErrorsPanel(QWidget* parent)
+    :QFrame(parent), m_ui(new Ui::ErrorsPanel)
+{
+    m_ui->setupUi(this);
+
+    m_ui->b_menu->setIcon(QIcon(":/icons/attention"));
+
+    connect(m_ui->b_menu, SIGNAL(clicked()),
+            this, SLOT(showMenu()));
+
+    m_ui->b_ok->setVisible(false);
+    m_ui->b_menu->setVisible(false);
+}
+
+void ErrorsPanel::errorAdded(QWidget* widget, QString message)
+{
+    m_actions += new ErrorAction(widget, message, this);
+    m_widgets += widget;
+    setErrorsCount();
+}
+
+void ErrorsPanel::errorRemoved(QWidget* widget)
+{
+    int index = m_widgets.indexOf(widget);
+    if (index != -1) {
+        m_actions.removeAt(index);
+        m_widgets.removeAt(index);
+
+        setErrorsCount();
+    }
+}
+
+void ErrorsPanel::hasErrors(bool yes)
+{
+    if (yes) {
+        m_ui->b_ok->setVisible(false);
+        m_ui->b_menu->setVisible(true);
+    }
+    else {
+        m_ui->b_ok->setVisible(true);
+        m_ui->b_menu->setVisible(false);
+    }
+}
+
+void ErrorsPanel::setErrorsCount()
+{
+    int errors = m_actions.size();
+    m_ui->b_menu->setText(QString::number(errors) + " " + tr("error(s)"));
+}
+
+void ErrorsPanel::showMenu()
+{
+    QList<QAction*> actions;
+    foreach(ErrorAction* action, m_actions)
+        actions += action;
+
+    QAction* action = QMenu::exec(actions, mapToGlobal(m_ui->b_menu->pos()));
+    if (action)
+        ((ErrorAction*)action)->gotoError();
+}
+
+ErrorsPanel::~ErrorsPanel()
+{
+    foreach(ErrorAction* action, m_actions)
+        delete action;
+    delete m_ui;
+}
---
+++ widgets/errorsPanel.h
@@ -0,0 +1,45 @@
+/**
+ *
+ * File: errorsPanel.h
+ * Author: Alexander Yarygin <yarygin.alexander@gmail.com>
+ *
+ **/
+
+#ifndef ERRORSPANEL_H
+#define ERRORSPANEL_H
+
+#include <QFrame>
+#include <QString>
+
+#include "errorAction.h"
+
+namespace Ui {
+    class ErrorsPanel;
+}
+
+class ErrorsPanel : public QFrame
+{
+    Q_OBJECT
+public:
+    ErrorsPanel(QWidget* parent = 0);
+    virtual ~ErrorsPanel();
+
+public slots:
+    void errorAdded(QWidget* widget, QString message);
+    void errorRemoved(QWidget* widget);
+
+    void hasErrors(bool yes);
+
+private slots:
+    void showMenu();
+
+private:
+    void setErrorsCount();
+
+    QList<ErrorAction*> m_actions;
+    QList<QWidget*> m_widgets;
+
+    Ui::ErrorsPanel* m_ui;
+};
+
+#endif // ERRORSPANEL_H
